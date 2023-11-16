882a9d3731cee1f2303ff9ba845e14c2d85b7507
mikemwakio <145694476+mikemwakio@users.noreply.github.com>
2023-11-16 07:41:23 +0300

    None

---
+++ Addition program.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int n1,n2,sum=0;
+printf("Enter the first number\n");
+scanf("%d",&n1);
+printf("Enter the second number\n");
+scanf("%d",&n2);
+{
+sum=n1+n2;
+}
+printf("Sum of %d and %d is %d\n",n1,n2,sum);
+}
